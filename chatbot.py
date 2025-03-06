517cb5f3423834ed4e937d76cc4197119241f415
phongsathornjanjamsai@gmail.com <phongsathornjanjamsai@gmail.com>
2025-03-06 10:33:16 +0700

    update chat bot

--- chatbot.py
+++ chatbot.py
@@ -1,2 +1,13 @@
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+api_key = os.getenv("Gemini_Key")
+genai.configure(api_key=api_key)
+
 def chatbot(user_input):
-    return f"AI: I received your message: {user_input}"
+    model = genai.GenerativeModel("gemini-1.5-flash")  
+    response = model.generate_content(user_input)
+    return response.text 
+
